47abc56ef759310ce0d74868e7a820054c246298
bano <145287884+epokbano@users.noreply.github.com>
2024-02-01 21:34:37 +0100

    Add files via upload

---
+++ jaja.py
@@ -0,0 +1,32 @@
+# Zad1. Napisz analogiczny słownik (książka) który pozwoli ci edytować dane 😉 Funkcje umieść w 2 pliku i zaimportuj funkcje do main.py. Jak dobrze opanujesz co się dzieje w twoim pierwszym skrypcie dopiero potem przejdź do Zad2 . f
+
+from abrakadabra import nigger, nigger2
+
+
+jaja = {
+    "jajaklucz1" : "jajawartosc1",
+    "jajaklucz2" : "jajawartosc2"
+}
+
+nigger(jaja, "jajaklucz1")
+
+
+
+
+# Zad 2. Hej a co powiesz na listę słowników! Napisz program biblioteka który pozwoli ci w pełni edytować listę słowników jak i same książki 😉 im więcej posiedzidzisz i popłaczesz nad zad 2 tym lepiej zrozumiesz materiał 😉. Podziel program na pliki:
+
+
+
+jaja2 = {
+    "jaja2klucz1" : "jaja2wartosc1",
+    "jaja2klucz2" : "jaja2wartosc2"
+}
+
+
+jaja3 = {
+    "jaja3klucz1" : "jaja3wartosc1",
+    "jaja3klucz2" : "jaja3wartosc2"
+}
+lst = [jaja, jaja2, jaja3]
+
+nigger2(lst, jaja, jaja2, jaja3, "jajawartosc1", "jajawartosc2", "jaja2wartosc1", "jaja2wartosc2", "jaja3wartosc1", "jaja3wartosc2")
